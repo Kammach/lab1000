2748b3c0c2f9d6242e2868ce513b6695165c7ddf
Kammach <153260807+Kammach@users.noreply.github.com>
2025-12-26 19:45:34 +0700

    Update lab10_3.cpp

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,42 +1,37 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
 #include <iostream>
 #include <iomanip>
-#include <fstream>
 #include <cmath>
+#include <string>
+#include <fstream>
 #include <sstream>
 
 using namespace std;
 
-string formatDouble(double x) {
-    ostringstream out;
-    out << fixed << setprecision(3) << x;
-    string s = out.str();
-    while (s.back() == '0') s.pop_back();
-    if (s.back() == '.') s.pop_back();
-    return s;
-}
+int main()
+{
+    int count = 0;
+    float sum = 0.0;
+    float sum_of_square = 0.0;
+    string textline;
 
-int main(){
-    ifstream file("score.txt");
+    ifstream source("score.txt");
 
-    double x, sum = 0.0, sum_sq = 0.0;
-    int N = 0;
+    while (getline(source, textline))
+    {
+        float x;
+        stringstream ss(textline);
+        ss >> x;
 
-    while (file >> x) {
         sum += x;
-        sum_sq += x * x;
-        N++;
+        sum_of_square += x * x;
+        count++;
     }
-    file.close();
 
-    double mean = sum / N;
-    double sd = sqrt((sum_sq / N) - (mean * mean));
-
-    cout << "Number of data = " << N << endl;
-    cout << "Mean = " << formatDouble(mean) << endl;
-    cout << "Standard deviation = " << formatDouble(sd) << endl;
+    cout << "Number of data = " << count << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << sum / count << "\n";
+    cout << "Standard deviation = "
+         << sqrt((sum_of_square / count) - pow(sum / count, 2));
 
     return 0;
 }
